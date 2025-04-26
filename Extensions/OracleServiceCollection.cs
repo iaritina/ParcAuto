66e649d2d9dbafbe5d42fceee0a57fdc5aa37f74
iaritina <iaritina7@gmail.com>
2025-04-26 12:36:30 +0300

    add Oracle database connection

---
+++ Extensions/OracleServiceCollection.cs
@@ -0,0 +1,17 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace ParcAuto.Extensions;
+
+public static class OracleServiceCollection
+{
+    public static IServiceCollection AddOracleConnection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<OracleConnection>(sp =>
+        {
+            var connectionString = configuration.GetConnectionString("OracleDb");
+            return new OracleConnection(connectionString);
+        });
+
+        return services;
+    }
+}
